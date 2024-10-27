6d67bae6fd6be737a322618af0093321a85e2179
Prashant Umak <umakprashant@gmail.com>
2024-10-27 15:37:25 +0530

    check working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LeftPanelComponent } from './component/left-panel/left-panel.component';
+import { MainLayoutComponent } from './component/main-layout/main-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LeftPanelComponent, MainLayoutComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'cricket-score-app';
---
+++ src/app/component/left-panel/left-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-left-panel',
+  standalone: true,
+  imports: [],
+  templateUrl: './left-panel.component.html',
+  styleUrl: './left-panel.component.scss'
+})
+export class LeftPanelComponent {
+
+}
